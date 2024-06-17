ca13d2e6f421c3f6e791b9043d412972b5173ff9
JaeSunEo <wotjs020708@gmail.com>
2024-06-17 14:40:55 +0900

    initialize Listview, 기본 리스트 뷰

---
+++ ListView/ListView/ListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewApp.swift
+//  ListView
+//
+//  Created by 어재선 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
