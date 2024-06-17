1ca0d70dd209a75387edc340ad3a6fd1defbfb30
JaeSunEo <wotjs020708@gmail.com>
2024-06-17 13:14:54 +0900

    initialize Project Management

---
+++ StateManagement/StateManagement/StateManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateManagementApp.swift
+//  StateManagement
+//
+//  Created by 어재선 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct StateManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
