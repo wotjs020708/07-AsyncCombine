0857dce30212be94e923aba053b6d0c1d160d140
JaeSunEo <wotjs020708@gmail.com>
2024-06-18 14:09:27 +0900

    기본 Publisher 실행

--- GettingStartedWithcombine.playground/Pages/PizzaOrdering.xcplaygroundpage/Contents.swift
+++ GettingStartedWithcombine.playground/Pages/PizzaOrdering.xcplaygroundpage/Contents.swift
@@ -3,6 +3,27 @@
 import Foundation
 import Combine
 
-var greeting = "Hello, playground"
+let pizzOrder = Order()
+
+let pizzaOrderPublisher = NotificationCenter.default.publisher(for: .didUpdateOrderStatus, object: pizzOrder)
+
+pizzaOrderPublisher.sink { notification in
+    print("---------------notification start-------------------")
+    dump(notification)
+    print("---------------notification end-------------------")
+}
+
+pizzaOrderPublisher.map { notification in
+    notification.userInfo?["status"] as? OrderStatus ?? OrderStatus.placing
+}
+.sink{ orderStatus in
+    print("Order status [\(orderStatus)]")
+}
+print("Order: \(pizzOrder.status)")
+
+NotificationCenter.default.post(name: .didUpdateOrderStatus,
+                                object: pizzOrder,
+                                userInfo: ["status":OrderStatus.processing])
+
 
 //: [Next](@next)
