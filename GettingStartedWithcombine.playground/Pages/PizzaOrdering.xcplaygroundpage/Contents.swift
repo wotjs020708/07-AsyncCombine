636eaa88373ba945f3efa1f76979836281a59f7f
JaeSunEo <wotjs020708@gmail.com>
2024-06-18 15:29:02 +0900

    CombineLatestm, Publisher 합성, Pizza Ordering3

--- GettingStartedWithcombine.playground/Pages/PizzaOrdering.xcplaygroundpage/Contents.swift
+++ GettingStartedWithcombine.playground/Pages/PizzaOrdering.xcplaygroundpage/Contents.swift
@@ -8,9 +8,12 @@ let pizzOrder = Order()
 let pizzaOrderPublisher = NotificationCenter.default.publisher(for: .didUpdateOrderStatus, object: pizzOrder)
 
 pizzaOrderPublisher.sink { notification in
-    print("---------------notification start-------------------")
-    dump(notification)
-    print("---------------notification end-------------------")
+    Task {
+        try? await Task.sleep(for: .seconds(2))
+        print("---------------notification start-------------------")
+        dump(pizzOrder)
+        print("---------------notification end-------------------")
+    }
 }
 
 pizzaOrderPublisher.map { notification in
@@ -19,6 +22,12 @@ pizzaOrderPublisher.map { notification in
 .sink{ orderStatus in
     print("Order status [\(orderStatus)]")
 }
+
+pizzaOrderPublisher.compactMap{ notification in
+    notification.userInfo?["status"] as? OrderStatus
+}
+.assign(to: \.status, on: pizzOrder)
+
 print("Order: \(pizzOrder.status)")
 
 NotificationCenter.default.post(name: .didUpdateOrderStatus,
