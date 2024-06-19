9507641eb721d8ab0f08a0e1d257e097f2c9c4f3
JaeSunEo <wotjs020708@gmail.com>
2024-06-19 11:43:34 +0900

    publisher 정리

--- SignUpForm/SignUpForm/AuthenticationService.swift
+++ SignUpForm/SignUpForm/AuthenticationService.swift
@@ -26,21 +26,15 @@ enum NetworkError: Error {
 
 class AuthenticationService {
 
-    func checkusernameAvailableNaive(userName: String) -> AnyPublisher<Bool, Never> {
+    func checkusernameAvailable(userName: String) -> AnyPublisher<Bool, Never> {
         guard let url = URL(string: "http://127.0.0.1:8080/isUserNameAvailable?userName=\(userName)") else {
             return Just(false).eraseToAnyPublisher()
         }
         
         return URLSession.shared.dataTaskPublisher(for: url)
-            .map{ data, response in
-                do {
-                    let decoder = JSONDecoder()
-                    let userAvailableMessage = try decoder.decode(UserNameAvailableMessage.self, from: data)
-                    return userAvailableMessage.isAvailable
-                } catch {
-                    return false
-                }
-            }
+            .map(\.data)
+            .decode(type: UserNameAvailableMessage.self, decoder: JSONDecoder())
+            .map(\.isAvailable)
             .replaceError(with: false)
             .eraseToAnyPublisher()
         
