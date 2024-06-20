9b01dfbb7ded1792974f5cc2e86da6595571bd62
JaeSunEo <wotjs020708@gmail.com>
2024-06-20 09:11:29 +0900

    initialize Project SignUpFrom2

--- SignUpForm/SignUpForm/AuthenticationService.swift
+++ SignUpForm/SignUpForm/AuthenticationService.swift
@@ -23,8 +23,17 @@ enum NetworkError: Error {
     
 }
 
+struct APIErrorMessage: Decodable {
+    var error: Bool
+    var reason: String
+}
+
 enum APIError: LocalizedError {
     case invalidRequestError(String)
+    case transportError(Error)
+    case invalidResponse
+    case validationError(String)
+    case decodingError(Error)
 }
 
 
@@ -36,6 +45,29 @@ class AuthenticationService {
         }
         
         return URLSession.shared.dataTaskPublisher(for: url)
+            .mapError{ error -> Error in
+                return APIError.transportError(error)
+            }
+            .tryMap{ (data, response) -> (data: Data, response: URLResponse) in
+            print("Received response from server, now checking status code")
+                guard let urlResponse = response as? HTTPURLResponse else {
+                    throw APIError.invalidResponse
+                }
+                if (200..<300) ~= urlResponse.statusCode {} else {
+                    let decoder = JSONDecoder()
+                    let apiError = try decoder.decode(APIErrorMessage.self, from: data)
+                    
+                    if urlResponse.statusCode == 400 {
+                        throw APIError.validationError(apiError.reason)
+                    }
+                    if (500..<600) ~= urlResponse.statusCode {
+                        let retryAfter = urlResponse.value(forHTTPHeaderField: "Retry-After")
+                        // TODO: SERVER ERROR HANDLING
+                    }
+                }
+                    return (data, response)
+            }
+        
             .map(\.data)
             .decode(type: UserNameAvailableMessage.self, decoder: JSONDecoder())
             .map(\.isAvailable)
