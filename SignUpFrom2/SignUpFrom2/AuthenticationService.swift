5da5a7f55da4ff449439adb56909795354c26474
JaeSunEo <wotjs020708@gmail.com>
2024-06-20 11:12:37 +0900

    AuthenticationService 추가

--- SignUpFrom2/SignUpFrom2/AuthenticationService.swift
+++ SignUpFrom2/SignUpFrom2/AuthenticationService.swift
@@ -11,7 +11,14 @@ import Combine
 struct AuthenticationService {
     
     func checkUserNameAvailablePublisher(userName: String) -> AnyPublisher<Bool, Error> {
-        return Fail(error: APIError.invalidResponse).eraseToAnyPublisher()
+        guard let url = URL(string: "http://127.0.0.1:8080/isUserNameAvailable?userName=\(userName)") else {
+            return Fail(error: APIError.invalidResponse).eraseToAnyPublisher()
+        }
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: UserNameAvailableMessage.self, decoder: JSONDecoder())
+            .map(\.isAvailable)
+            .eraseToAnyPublisher()
     }
     
 }
