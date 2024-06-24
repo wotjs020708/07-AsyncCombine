396b872e9cbe1a18ae7c2420c5f21548605b15f5
JaeSunEo <wotjs020708@gmail.com>
2024-06-24 11:40:07 +0900

    async await

---
+++ AsyncAwait.playground/Pages/AsyncAwait.xcplaygroundpage/Contents.swift
@@ -0,0 +1,61 @@
+import Foundation
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+public func customerSays(_ message: String) {
+    print("[Customer] \(message)")
+}
+
+public func sandwichMakerSays(_ message: String, waitFor time: UInt32 = 0) {
+    print("[Sandwich maker] \(message)")
+    if time > 0 {
+        print("                 ... this will take \(time)s")
+        sleep(time)
+    }
+}
+
+func toastBread(_ bread: String) async -> String {
+    sandwichMakerSays("Toasting the bread... Standing by...")
+    try? await Task.sleep(nanoseconds: 5_000_000_000)
+    return "Cripy \(bread)"
+}
+
+func slice(_ ingredients: [String]) async -> [String] {
+    var result = [String]()
+    for ingredient in ingredients {
+        sandwichMakerSays("Slicing \(ingredient)")
+        try? await Task.sleep(for: .seconds(1))
+        result.append("sliced \(ingredient)")
+        
+    }
+    return result
+}
+
+func makeSandwich(bread: String, ingredients: [String], condiments: [String]) async -> String {
+    sandwichMakerSays("Preparing your sandwich...")
+    
+    let toasted = await toastBread(bread)
+    let sliced = await slice(ingredients)
+    
+    sandwichMakerSays("Spreading \(condiments.joined(separator: ", and ")) om \(toasted)")
+        sandwichMakerSays("Layering \(sliced.joined(separator: ", "))")
+        sandwichMakerSays("Putting lettuce on top")
+        sandwichMakerSays("Putting another slice of bread on top")
+
+        return "\(ingredients.joined(separator: ", ")), \(condiments.joined(separator: ", ")) on \(toasted)"
+}
+
+let clock = ContinuousClock()
+
+sandwichMakerSays("Hello to Cafe Async, where we execute your order in asynchronously.")
+sandwichMakerSays("Please place your order.")
+
+Task {
+    let time = await clock.measure {
+        let sandwich = await makeSandwich(bread: "Rye", ingredients: ["Cucmbers", "Tomatoes"], condiments: ["Mayo", "Mustard"])
+        customerSays("Hmmm.... this looks like a delicious \(sandwich) sandwich!")
+        print("The end.")
+    }
+    print("Making this sandwich took \(time)")
+}
